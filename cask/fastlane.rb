043d50b2f362ec7b2fc338e5f5a96381685c283d
Manu Wallner <m.wallner@xforge.at>
2016-11-17 11:44:56 -0800

    Update fastlane cask definition

--- cask/fastlane.rb
+++ cask/fastlane.rb
@@ -2,7 +2,7 @@ cask 'fastlane' do
   VERSION_NUMBER='1.0'
 
   version VERSION_NUMBER
-  sha256 '7ce7c6f200ca54b33d1bb9a175feb8517d547745a1e01ad08c9137d7d9600133'
+  sha256 '0130980b94034b163e7d45d50bfe114af5345888267572edad640fe1b2d66ea5'
 
   url "https://kits-crashlytics-com.s3.amazonaws.com/fastlane/standalone/bundle-#{VERSION_NUMBER}.zip"
   name 'fastlane'
