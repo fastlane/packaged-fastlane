b3b35ad1dd6e5d6a08b50153cc7eaa3818db90f4
Manu Wallner <m.wallner@xforge.at>
2016-11-17 17:46:34 -0800

    Updated cask definitions to address review

--- cask/fastlane.rb
+++ cask/fastlane.rb
@@ -1,11 +1,9 @@
 cask 'fastlane' do
-  VERSION_NUMBER = '1.0'.freeze
-
-  version VERSION_NUMBER
+  version '1.0'
   sha256 'f642cdae3d4841f85d4a9260866361d136fad82974cf5905629d48f70d86f294'
 
   # kits-crashlytics-com.s3.amazonaws.com/fastlane/ was verified as official when first introduced to the cask
-  url "https://kits-crashlytics-com.s3.amazonaws.com/fastlane/standalone/bundle-#{VERSION_NUMBER}.zip"
+  url "https://kits-crashlytics-com.s3.amazonaws.com/fastlane/standalone/bundle-#{version}.zip"
   name 'fastlane'
   homepage 'https://fastlane.tools'
 
